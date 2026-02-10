83ee53c28186de03132f35f9b1c7e96eb4226dc5
Muhammadxoja <muhammadxojaxiaomi@gmail.com>
2026-02-10 11:14:30 +0500

    fix 3

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,5 +1,13 @@
 import serverless from "serverless-http";
 import app from "../../server/src/app";
+import { ensureSchema, getDb } from "../../server/src/db";
+
+try {
+  ensureSchema();
+  getDb();
+} catch (error) {
+  console.error("Failed to initialize database:", error);
+}
 
 export const handler = serverless(app, {
   basePath: "/.netlify/functions/api"
